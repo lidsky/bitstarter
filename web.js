ebbd47a7369a193f4658d24c74d8fdd4492c0f83
lidsky <hmr1808@gmail.com>
2013-07-12 02:28:38 +0000

    updated fs.write fn in web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var out = fs.readFile('index.html');
+var out = fs.readFileSync('index.html');
 
 if (typeof(out) !== 'string'){
     var buffer = new Buffer(out,'utf-8');
